48e215c51d1903364e5a0f5dccd75a6827c06a22
Emm Ajayi <truevirtue001@yahoo.com>
2024-11-15 14:12:08 +0100

    Finish up

--- product/product_service.js
+++ product/product_service.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
+const path = require('path');
 const port = 9001;
 
 app.set('views', path.join(__dirname, 'pages'));
@@ -7,12 +8,12 @@ app.set('view engine', 'ejs');
 app.use(express.static('static'));
 
 const products = [
-    { id: 1, name: 'Women Boots', price: 320, image: '/images/brown-boots.jpg' },
-    { id: 2, name: 'Desktop Computer', price: 499, image: '/images/desktop-comp.jpg' },
+    { id: 1, name: 'Sport Car', price: 189250, image: '/images/red-car.jpg' },
+    { id: 2, name: 'Samsung Smartphone', price: 430, image: '/images/samsung-phone.jpg' },
     { id: 3, name: 'House', price: 989244, image: '/images/house.jpg' },
-    { id: 4, name: 'Sport Car', price: 250, image: '/images/red-car.jpg' },
-    { id: 5, name: 'Samsung Smartphone', price: 430, image: '/images/samsung-phone.jpg' },
-    { id: 6, name: 'Beverage Drink', price: 300, image: '/images/smoothie.jpg' },
+    { id: 4, name: 'Beverage Drink', price: 300, image: '/images/smoothie.jpg' },
+    { id: 5, name: 'Women Boots', price: 320, image: '/images/brown-boots.jpg' },
+    { id: 6, name: 'Desktop Computer', price: 499, image: '/images/desktop-comp.jpg' },
 ];
 
 app.get('/api/products', (req, res) => {
--- user/user_service.js
+++ user/user_service.js
@@ -1,17 +1,19 @@
 const express = require('express');
 const app = express();
+const path = require('path'); 
 const port = 9003;
 
 app.set('views', path.join(__dirname, 'pages'));
 app.set('view engine', 'ejs');
+app.use(express.static('static'));
 
 const users = [
-    { id: 1, name: 'Adekunle Johnson', email: 'dexjohnson4real@providusbank.com', age: 28 },
+    { id: 1, name: 'Taribo West', email: 'sir_taribo_Westa@nff.com', age: 99 },
     { id: 2, name: 'Ngozi Okoli', email: 'ngbabe111@microsoft.com', age: 26 },
     { id: 3, name: 'Faith Pamah', email: 'justfaith1k@gmail.com.com', age: 21 },
     { id: 4, name: 'Fatima Yaro', email: 'fat_Yaro22@yahoo.com', age: 25 },
     { id: 5, name: 'Fred Collins', email: 'collinsfred@mail.com', age: 30 },
-    { id: 6, name: 'Taribo West', email: 'sir_taribo_Westa@nff.com', age: 99 },
+    { id: 6, name: 'Adekunle Johnson', email: 'dexjohnson4real@providusbank.com', age: 28 },
 ];
 
 app.get('/api/users', (req, res) => {
