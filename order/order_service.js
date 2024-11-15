c307758a5d365f75f6d5ecf63509d32f2dd65dcf
Emm Ajayi <truevirtue001@yahoo.com>
2024-11-15 14:10:19 +0100

    Finish up

--- order/order_service.js
+++ order/order_service.js
@@ -1,17 +1,19 @@
 const express = require('express');
 const app = express();
+const path = require('path');
 const port = 9002;
 
 app.set('views', path.join(__dirname, 'pages'));
 app.set('view engine', 'ejs');
+app.use(express.static('static'));
 
 const orders = [
-    { id: 1, productId: 1, quantity: 44 },
-    { id: 2, productId: 2, quantity: 120 },
-    { id: 3, productId: 3, quantity: 6 },
-    { id: 4, productId: 4, quantity: 10 },
-    { id: 5, productId: 5, quantity: 39 },
-    { id: 6, productId: 5, quantity: 1 },
+    { id: 1, productId: 1, product: 'Sport Car', quantity: 44, buyer: 'Fatima Yaro' },
+    { id: 2, productId: 2, product: 'Samsung Smartphone', quantity: 120, buyer: 'Taribo West' },
+    { id: 3, productId: 3, product: 'House', quantity: 6, buyer: 'Faith Pamah' },
+    { id: 4, productId: 4, product: 'Beverage Drink', quantity: 10, buyer: 'Adekunle Johnson' },
+    { id: 5, productId: 5, product: 'Women Boots', quantity: 39, buyer: 'Fred Collins' },
+    { id: 6, productId: 6, product: 'Desktop Computer', quantity: 1, buyer: 'Ngozi Okoli', },
 ];
 
 app.get('/api/orders', (req, res) => {
